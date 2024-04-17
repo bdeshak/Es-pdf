4aedd322a6bba6924ed09c5f4ec15d7e9efd5898
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-04-17 08:25:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,16 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mergeRouter);
+/*
 app.use(splitRouter);
 app.use(editorRouter);
 app.use(watermarkRouter);
 app.use(deleteRouter);
 app.use(pageNoRoute);
 app.use(rotatePdf);
+*/
 app.use(pdfToWord);
+/*
 app.use(pdfToExcel);
 app.use(pdfToPpt);
 app.use(pdfToPng);
@@ -48,6 +51,7 @@ app.use(pdfToJson)
 app.use(pdfToTiff)
 app.use(pdfToTxt);
 app.use(wordToPdf)
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080, () => {
